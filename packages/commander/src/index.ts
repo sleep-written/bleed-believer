8eb14b2db2a0acd93149cc150df433d8f7a54ac2
sleep-written <sleep.written0000@gmail.com>
2022-03-23 10:18:04 -0300

    Change the exposed elements of @bleed-believer/commander

--- packages/commander/src/index.ts
+++ packages/commander/src/index.ts
@@ -1,6 +1,20 @@
-export * from './command';
-export * from './interfaces';
-export * from './command-routing';
+export {
+    Command,
+    CommandOptions,
+    CommandDecorator
+} from './command';
 
+export {
+    CommandRouting,
+    CommandRoutingOptions,
+    CommandRoutingDecorator
+} from './command-routing';
+
+export {
+    Argv,
+    ArgvData,
+    ParserOptions
+} from './argv';
+
+export * from './interfaces';
 export { Commander } from './commander';
-export { Argv, ArgvData, ParserOptions } from './argv';
