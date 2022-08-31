ede39a6639d787323b451ec61810c45928ae8d30
sleep-written <sleep.written0000@gmail.com>
2022-08-31 11:48:09 -0400

    Added documentation in: - state-form. - state.

--- packages/state/src/state-form/state-form.ts
+++ packages/state/src/state-form/state-form.ts
@@ -2,8 +2,19 @@ import { filter, Observable, map } from 'rxjs';
 import { FormControl, FormGroup } from '@angular/forms';
 import { StateFormStruct } from './interfaces/index.js';
 
+/**
+ * An extension of `FormGroup` from [@angular/forms](https://angular.io/api/forms/FormGroup)
+ * with alternative methods to change its internal value and listen for changes.
+ */
 export class StateForm<T extends Record<string, any>> extends FormGroup {
     #ready = true;
+
+    /**
+     * This is a variant of `this.valueChanges`, but this observer ignores all
+     * calls from `this.setValueSilently` and `this.pathValuesSilently`. The
+     * value emitted by this observer removes all keys with `null` or `undefined`
+     * values.
+     */
     get valueChangesByUser(): Observable<Partial<T>> {
         return this
             .valueChanges
@@ -23,6 +34,10 @@ export class StateForm<T extends Record<string, any>> extends FormGroup {
             );
     }
 
+    /**
+     * This is a variant of `this.value`, but instead removes
+     * all keys with `null` or `undefined` values.
+     */
     get partialValue(): Partial<T> {
         const out: any = {};
         Object
@@ -38,6 +53,11 @@ export class StateForm<T extends Record<string, any>> extends FormGroup {
         return out;
     }
 
+    /**
+     * Create a new instance of `StateForm`, equivalent a normal `FormGroup`.
+     * @param struct An object with the keys and initial control configuration,
+     * exactly the same format using with `FormBuilder`.
+     */
     constructor(struct: StateFormStruct<T>) {
         super({});
 
@@ -55,12 +75,28 @@ export class StateForm<T extends Record<string, any>> extends FormGroup {
             });
     }
 
+    /**
+     * This is a variant of `this.setValue` method, and this should be used in
+     * conjunction with `this.valueChangesByUser` observer. This method sets
+     * the value of the entire form. The difference is:
+     * - Emits the change in `this.valueChanges`.
+     * - __Doesn't emits the change__ in `this.valueChangesByUser`.
+     * @param v The value to be setted to the whole form.
+     */
     setValueSilently(v: { [K in keyof T]: T[K] | null; }): void {
         this.#ready = false;
         this.setValue(v);
         this.#ready = true;
     }
 
+    /**
+     * This is a variant of `this.patchValue` method, and this should be used in
+     * conjunction with `this.valueChangesByUser` observer. This method sets
+     * the value of the entire form. The difference is:
+     * - Emits the change in `this.valueChanges`.
+     * - __Doesn't emits the change__ in `this.valueChangesByUser`.
+     * @param v The name of the controls and their values to be changed.
+     */
     patchValueSilently(v: Partial<{ [K in keyof T]: T[K] | null; }>): void {
         this.#ready = false;
         this.patchValue(v);
--- packages/state/src/state.ts
+++ packages/state/src/state.ts
@@ -1,6 +1,14 @@
 import { BehaviorSubject } from 'rxjs';
 import { Serial } from './serial.js';
 
+/**
+ * A class to create other classes as __State Manager__.
+ * 
+ * __How to use:__
+ * - Create a class (if you are using `Angular`, we recommend create a service instead).
+ * - Extends that class with this class.
+ * - Declare a constructor, passing a initial state to the `super` (the parent's constructor).
+ */
 export class State<T> {
     #serial = new Serial();
     #value: T;
