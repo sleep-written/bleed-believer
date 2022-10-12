1590dbee4da5bd01d20275fb770e9eab729bca56
sleep-written <sleep.written0000@gmail.com>
2022-10-12 12:49:45 -0300

    Changes: - Added the capacity to use BehabiorSubject<T> or Subject<T>.

--- packages/state/src/state.ts
+++ packages/state/src/state.ts
@@ -1,23 +1,35 @@
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { Serial } from './serial.js';
 
 /**
  * A class to create other classes as __State Manager__.
  * 
  * __How to use:__
- * - Create a class (if you are using `Angular`, we recommend create a service instead).
- * - Extends that class with this class.
- * - Declare a constructor, passing a initial state to the `super` (the parent's constructor).
+ * - Create a service (using CLI: `npx ng g s path/to/the/service`)
+ * - Extends that service class with this class (necesary if you wants to use
+ * `BehaviorSubject<T>`).
+ * - Declare a constructor and call to super to initialize this class.
+ * 
+ * If you call `super()` passing a value, the `this.state` property will
+ * be a `BehaviorSubject<T>`, otherwise will be a `Subject<T>` instance.
+ * The differences are:
+ * - `BehaviorSubject<T>` will be initialized inmediatly emitting a value
+ *   when the class is instantiated.
+ * - `Subject<T>` waits for changes emitted manually by the developer.
  */
 export class State<T> {
     #serial = new Serial();
-    #value: T;
+    #value!: T;
 
-    readonly state: BehaviorSubject<T>;
+    readonly state: Subject<T> | BehaviorSubject<T>;
 
-    constructor(initial: T) {
-        this.#value = initial;
-        this.state =  new BehaviorSubject<T>(initial);
+    constructor(initial?: T) {
+        if (initial != null) {
+            this.#value = initial;
+            this.state = new BehaviorSubject<T>(initial);
+        } else {
+            this.state = new Subject<T>();
+        }
 
         Object.defineProperty(this, 'context', {
             configurable: false,
