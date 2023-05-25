e0a8a97de0aca29a00d9ecc85fbbb5d32652efab
sleep-written <sleep.written0000@gmail.com>
2023-05-25 15:48:34 -0400

    Changes @bleed-believer/path-alias: - Added the ability to search the path of it's own loader. This is   needed if you want to install the package globally.

--- packages/path-alias/src/cli.ts
+++ packages/path-alias/src/cli.ts
@@ -1,20 +1,35 @@
 #! /usr/bin/env node
+import { fileURLToPath } from 'url';
 import { spawn } from 'child_process';
+import { join } from 'path';
+
 import { logger, separator } from './logger.js';
 
 logger.info('Starting... ⤵');
 separator();
 
+// Getting the loader path
+const loaderPath = join(
+    fileURLToPath(import.meta.url),
+    '..', 'index.mjs'
+);
+
+// Execute the program as a child process
 await new Promise<void>((resolve, reject) => {
-    const argv = [
-        '--no-warnings',
-        '--loader=@bleed-believer/path-alias',
-        ...process.argv.slice(2)
-    ];
-    
-    const proc = spawn('node', argv, { stdio: 'inherit' });
-    proc.on('close', ___ => resolve());
-    proc.on('error', err => reject(err))
+    try {
+        const argv = [
+            '--no-warnings',
+            `--loader`,
+            loaderPath,
+            ...process.argv.slice(2)
+        ];
+        
+        const proc = spawn('node', argv, { stdio: 'inherit' });
+        proc.on('close', ___ => { resolve(); });
+        proc.on('error', err => { reject(err); });
+    } catch (err) {
+        reject(err);
+    }
 })
     .then(_ => {
         separator();
