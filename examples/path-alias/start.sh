0b0f365091093b7e80f6290ebdecf64ec7686c5b
sleep-written <sleep.written0000@gmail.com>
2023-11-05 01:23:25 -0300

    Changes: - Updated the "path-alias" example.

--- examples/path-alias/start.sh
+++ examples/path-alias/start.sh
@@ -3,6 +3,5 @@ clear
 
 #Execute the program with the loader
 node \
---no-warnings \
---loader ../../packages/path-alias/dist/loader/index.mjs \
+--import ../../packages/path-alias/dist/index.js \
 $1
